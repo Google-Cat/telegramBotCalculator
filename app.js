ea86a51c84857c5d8d61723e6573fbab9ddc5183
Леонид Лунин <leonidlunin@MacBook-Pro-Leonid.local>
2018-02-23 23:27:49 +0100

    try to change message by click

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var port = process.env.PORT || 8080;
 //create a server object:
 http.createServer(function (req, res) {
     var objekt;
-    var toSendMessage;
+    var command;
     var inComeMessage;
+    var query;
     var str = '';
     var body = [];
     req.on('data', function (chunk) {
@@ -16,39 +17,45 @@ http.createServer(function (req, res) {
         str = body.toString();
         objekt = JSON.parse(str);
         inComeMessage = objekt.message;
-        console.log(inComeMessage.text.toString());
-        if (inComeMessage.text.toString() === "/start") {
-            toSendMessage = {
-                "method": 'sendMessage',
-                "chat_id": inComeMessage.chat.id.toString(),
-                "text": 'Вот тебе твоя клава!',
-                "reply_markup": {
-                    'inline_keyboard': [[{"text": 'AC', "callback_data": "reset"}, {
-                        "text": '+',
-                        "callback_data": "summ"
-                    }, {"text": '-', "callback_data": "minus"}],
-                        [{"text": '7', "callback_data": 7}, {"text": '8', "callback_data": 8}, {
-                            "text": '9',
-                            "callback_data": 9
-                        }],
-                        [{"text": '4', "callback_data": 4}, {"text": '5', "callback_data": 5}, {
-                            "text": '6',
-                            "callback_data": 6
-                        }],
-                        [{"text": '1', "callback_data": 1}, {"text": '2', "callback_data": 2}, {
-                            "text": '3',
-                            "callback_data": 3
-                        }],
-                        [{"text": '0', "callback_data": 0}]]
-                }
-            };
+        query = objekt.callback_query;
+        if (query === undefined) {
+            if (inComeMessage.text !== undefined) {
+                console.log(inComeMessage.text.toString());
+                if (inComeMessage.text.toString() === "/start") {
+                    command = {
+                        "method": 'sendMessage',
+                        "chat_id": inComeMessage.chat.id.toString(),
+                        "text": '0',
+                        "reply_markup": {
+                            'inline_keyboard': [[{"text": 'AC', "callback_data": "reset"}, {
+                                "text": '+',
+                                "callback_data": "summ"
+                            }, {"text": '-', "callback_data": "minus"}],
+                                [{"text": '7', "callback_data": 7}, {"text": '8', "callback_data": 8}, {
+                                    "text": '9',
+                                    "callback_data": 9
+                                }],
+                                [{"text": '4', "callback_data": 4}, {"text": '5', "callback_data": 5}, {
+                                    "text": '6',
+                                    "callback_data": 6
+                                }],
+                                [{"text": '1', "callback_data": 1}, {"text": '2', "callback_data": 2}, {
+                                    "text": '3',
+                                    "callback_data": 3
+                                }],
+                                [{"text": '0', "callback_data": 0}]]
+                        }
+                    };
+                } else
+                    command = {
+                        "method": 'sendMessage',
+                        "chat_id": inComeMessage.chat.id.toString(),
+                        "text": 'Не пиши мне ничего'
+                    };
+            }
         } else
-            toSendMessage = {
-                "method": 'sendMessage',
-                "chat_id": inComeMessage.chat.id.toString(),
-                "text": 'Не пиши мне ничего'
-            };
-        res.write(JSON.stringify(toSendMessage));
+            command = {"method": "editTextMessage", "message_id": query.message.message_id, "text": query.data}
+        res.write(JSON.stringify(command));
         res.end();
     });
 }).listen(port); //the server object listens on port 8080
