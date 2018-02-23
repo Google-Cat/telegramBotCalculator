b771a55e59e423ebfe4a219320d2d34032b63769
Леонид Лунин <leonidlunin@MacBook-Pro-Leonid.local>
2018-02-23 17:32:43 +0100

    resize keyboard

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ http.createServer(function (req, res) {
                 "chat_id": inComeMessage.chat.id.toString(),
                 "text": 'Вот тебе твоя клава!',
                 "reply_markup": {
-                    'keyboard': [[{"text": 'AC'}, {"text": '+'}, {"text": '-'}], [{"text": '7'}, {"text": '8'}, {"text": '9'}], [{"text": '4'}, {"text": '5'}, {"text": '6'}], [{"text": '1'}, {"text": '2'}, {"text": '3'}], [{"text": ''}, {"text": '0'}, {"text": ''}]]
+                    'keyboard': [[{"text": 'AC'}, {"text": '+'}, {"text": '-'}], [{"text": '7'}, {"text": '8'}, {"text": '9'}], [{"text": '4'}, {"text": '5'}, {"text": '6'}], [{"text": '1'}, {"text": '2'}, {"text": '3'}], [{"text": ''}, {"text": '0'}, {"text": ''}]],
+                    'resize_keyboard': true
                 }
             };
         } else
