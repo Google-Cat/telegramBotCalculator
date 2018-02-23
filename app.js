4b6a4cf6aa4862492715425d74cdfbe994fcd157
Леонид Лунин <leonidlunin@MacBook-Pro-Leonid.local>
2018-02-23 23:05:07 +0100

    callbackquery for all buttons

--- app.js
+++ app.js
@@ -27,10 +27,19 @@ http.createServer(function (req, res) {
                         "text": '+',
                         "callback_data": "summ"
                     }, {"text": '-', "callback_data": "minus"}],
-                        [{"text": '7'}, {"text": '8'}, {"text": '9'}],
-                        [{"text": '4'}, {"text": '5'}, {"text": '6'}],
-                        [{"text": '1'}, {"text": '2'}, {"text": '3'}],
-                        [{"text": ' '}, {"text": '0'}, {"text": ' '}]]
+                        [{"text": '7', "callback_data": 7}, {"text": '8', "callback_data": 8}, {
+                            "text": '9',
+                            "callback_data": 9
+                        }],
+                        [{"text": '4', "callback_data": 4}, {"text": '5', "callback_data": 5}, {
+                            "text": '6',
+                            "callback_data": 6
+                        }],
+                        [{"text": '1', "callback_data": 1}, {"text": '2', "callback_data": 2}, {
+                            "text": '3',
+                            "callback_data": 3
+                        }],
+                        [{"text": '0', "callback_data": 0}]]
                 }
             };
         } else
