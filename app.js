91b8c24808394576ef4c57be501b294c519f4d87
Леонид Лунин <leonidlunin@MacBook-Pro-Leonid.local>
2018-02-23 23:40:53 +0100

    fix crash

--- app.js
+++ app.js
@@ -53,13 +53,14 @@ http.createServer(function (req, res) {
                         "text": 'Не пиши мне ничего'
                     };
             }
-        } else
+        } else {
             command = {
                 "method": "editMessageText",
                 "message_id": query.message.message_id,
                 "text": "потыкал? ну теперь заживём"
             };
-        console.log(command.method, command.message.message_id, command.text);
+            console.log(command.method, command.message.message_id, command.text);
+        }
         res.write(JSON.stringify(command));
         res.end();
     });
